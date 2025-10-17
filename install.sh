ce2f65dfb7fa18f29ea014431fc2cd50292558a8
Jovali99 <joakimvad@gmail.com>
2025-10-17 15:20:17 +0200

    Updated full install script

--- install.sh
+++ install.sh
@@ -1,21 +1,24 @@
 #!/usr/bin/env bash
-echo "Updating system packages..."
+echo -e "Updating system packages..."
 apt update
 apt install parallel
 
 
 if [ -d "venv" ]; then
-    echo "Virtual environment already exists. Skipping creation."
+    echo -e "Virtual environment already exists. Skipping creation."
 else
-    echo "Creating Python virtual environment..."
+    echo -e "Creating Python virtual environment..."
     python3 -m venv venv
 fi
 source venv/bin/activate
 
-echo "Upgrading pip..."
+echo -e "Upgrading pip..."
 pip install --upgrade pip
 
-echo "Installing project dependencies from pyproject.toml..."
+echo -e "Installing project dependencies from pyproject.toml..."
 pip install .
 
-echo "Installation complete!"
+echo -e "Installing torch!"
+pip install torch==2.7.0+cu126 torchvision==0.22.0+cu126 --index-url https://download.pytorch.org/whl/cu126
+
+echo -e "Installation complete!"
