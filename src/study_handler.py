d24e5dd4306b85d15115b8660db405e660d5dc7d
Jovali99 <joakimvad@gmail.com>
2025-11-24 23:39:21 +0100

    Initial setup of the fbd optimization

--- src/study_handler.py
+++ src/study_handler.py
@@ -1,4 +1,12 @@
-from src.dataset_handler import get_dataloaders
+from cifar_handler import CifarInputHandler
+from src.dataset_handler import get_dataloaders, get_weighted_dataloaders
+from src.models.resnet18_model import ResNet18
+from utils import sigmoid_weigths, calculate_logits
+from torch import nn, optim
+from LeakPro.leakpro.attacks.mia_attacks.lira import lira_vectorized
+from LeakPro.leakpro.attacks.mia_attacks.rmia import rmia_vectorised
+
+import numpy as np
 import torch
 import torch.nn.functional as F
 import torchvision
@@ -61,3 +69,72 @@ def objective(trial):
             best_val_accuracy = val_accuracy
 
     return best_val_accuracy
+
+def fbd_objective(trial, norm_scores, trainset, testset, cfg, shadow_logits, shadow_inmask):
+    """
+        noise_std: Trial between [0.001, 0.1]
+        Centrality: Trial stepped between [0.0, 1.0]
+        Temperature: Trial between [0.05, 0.5]
+    """
+    # study params
+    noise_std = trial.suggest_float("noise_std", 1e-3, 1e-1)
+    centrality = trial.suggest_float("centrality", 0.0, 1.0, step=0.1)
+    temperature = trial.suggest_float("temperature", 5e-2, 5e-1)
+
+    weights = sigmoid_weigths(norm_scores, centrality, temperature)
+
+    lr = cfg["fbd_study"]["learning_rate"]
+    weight_decay = cfg["fbd_study"]["weight_decay"]
+    epochs = cfg["fbd_study"]["epochs"]
+    momentum = cfg["fbd_study"]["momentum"]
+    t_max = cfg["fbd_study"]["t_max"]
+    batch_size = cfg["fbd_study"]["batch_size"]
+    attack = cfg["fbd_study"]["attack"]
+
+    if(cfg["data"]["dataset"] == "cifar10" or cfg["data"]["dataset"] == "cinic10"):
+        num_classes = 10
+    elif(cfg["data"]["dataset"] == "cifar100"):
+        num_classes = 100
+    else:
+        raise ValueError(f"Incorrect dataset {cfg['data']['dataset']}")
+
+    model = ResNet18(num_classes=num_classes)
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay,)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=t_max)
+
+    train_loader, test_loader = get_weighted_dataloaders(batch_size, trainset, testset, weights)
+
+    CifarInputHandler().trainStudyFbD(train_loader, model, criterion, optimizer, epochs,
+                               noise_std, scheduler)
+
+    test_accuracy = CifarInputHandler().eval(test_loader, model, criterion).accuracy
+
+    assert trainset.dataset is testset.dataset, "trainset.dataset =/= testset.dataset"
+    full_dataset = trainset.dataset
+
+    model.to(DEVICE)
+    target_logits = calculate_logits(model, full_dataset, DEVICE)
+
+    # TODO RESCALE LOGITS
+    rescaled_target_logits = target_logits
+    # -------------
+
+    if(attack == "lira"):
+        scores = lira_vectorized(rescaled_target_logits,
+                                 shadow_logits,
+                                 shadow_inmask,
+                                 var_calculation="individual_carlini",
+                                 online=True)
+    elif(attack == "rmia"):
+        scores = rmia_vectorised(rescaled_target_logits, shadow_logits, shadow_inmask, online=True)
+    else:
+        raise ValueError(f"Incorrect attack parameter{cfg["fbd_study"]['attack']}")
+
+    # TODO calculate vulnerability
+    vulnerability = np.mean(scores)
+    # ---------------
+    return test_accuracy, vulnerability
+
+
--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,11 @@
 from LeakPro.leakpro.attacks.mia_attacks.lira import lira_vectorized
 from sklearn.metrics import roc_curve
+from torch.utils.data import Dataset, DataLoader, Subset
+from tqdm import tqdm
+
 import numpy as np
+import torch
+
 
 def print_yaml(data, indent=0):
     """Recursively print YAML."""
@@ -63,3 +68,76 @@ def interpolate_unique(fpr0, fpr, tpr, extrapolate=np.nan):
 
     return np.interp(fpr0, x, y, left=extrapolate, right=extrapolate)
 
+def sigmoid_weigths(score: np.ndarray, centrality: float, temperature: float) -> np.ndarray:
+    exp = np.exp((score-centrality)/temperature)
+    weight = 1.0/(1.0+exp)
+    return weight
+
+def calculate_logits(model, dataset, device, batch_size=128) -> np.ndarray:
+    model.eval()
+    logits_list = []
+    loader = DataLoader(dataset, batch_size=batch_size, shuffle=False)
+
+    with torch.no_grad():
+        for x, _ in tqdm(loader):
+            x = x.to(device)
+            out = model(x)      # logits
+            logits_list.append(out.cpu().numpy())
+
+    logits = np.concatenate(logits_list, axis=0)
+    return logits
+
+def percentile_score_normalization(scores: np.ndarray, percentile: int, eps: float = 1e-12) -> np.ndarray:
+    """
+    Normalize scores using percentile clipping.
+    
+    Args:
+        scores: Array of base model audit scores.
+        percentile: Percentile to clip extremes (e.g., 2 will clip lower 2% and upper 98%).
+        eps: Small number to prevent division by zero.
+        
+    Returns:
+        Normalized scores in [0, 1].
+    """
+    if(percentile > 50):
+        percentile = 100 - percentile
+    lo = np.percentile(scores, percentile)
+    hi = np.percentile(scores, 100-percentile)
+    norm = (scores - lo) / (hi - lo + eps)
+    return np.clip(norm, 0.0, 1.0)
+
+def print_percentiles(threshold, scores):
+    """
+    prints the percentiles from a set threshold
+
+    args:
+        threshold: Value threshold to print percentiles outside it
+        scores: The scores, either normalized or normal
+    """
+    threshold = 5  # change this to whatever you want
+    num_above = np.sum(scores > threshold)
+    print(f"Number of scores above {threshold}: {num_above}")
+
+    percent_above = 100 * num_above / len(scores)
+    print(f"Percentage of scores above {threshold}: {percent_above:.2f}%")
+
+    threshold_2 = -threshold  # change this to whatever you want
+    num_below = np.sum(scores < threshold_2)
+    print(f"Number of scores below {threshold_2}: {num_below}")
+
+    percent_below = 100 * num_below / len(scores)
+    print(f"Percentage of scores above {threshold_2}: {percent_below:.2f}%")
+
+def get_shadow_signals(shadow_logits, shadow_inmask, amount):
+    total_models = shadow_logits.shape[1]
+
+    if amount > total_models:
+        raise ValueError(f"Requested {amount} shadow models but only {total_models} available.")
+
+    # Choose random shadow model indices
+    selected_indices = np.random.choice(total_models, size=amount, replace=False)
+
+    # Select those columns
+    logits_sub = shadow_logits[:, selected_indices]
+    inmask_sub = shadow_inmask[:, selected_indices]
+    return logits_sub, inmask_sub
